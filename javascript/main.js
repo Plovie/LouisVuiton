64bbf7ec2a39fa801d68adece5afa95b2301d7da
nicol <nicolas>
2017-06-15 18:55:05 +0200

    commit last

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+$(document).foundation();
+
+// var database = firebase.database();
+// function writeUserData(userId, name, email, imageUrl) {
+//     database.ref('users/' + userId).set({
+//         username: name,
+//         email: email,
+//         profile_picture : imageUrl
+//     });
+// }
+//
+// function updateName(userId,name) {
+//     database.ref('users/' + userId).update({
+//         username: name,
+//     });
+// }
+//
+// function deleteById(userId) {
+//     database.ref('users/' + userId).remove({
+//     });
+//}
+
+
+//updateName(12,'coucou')
+//deleteById(12);
+//writeUserData(12,'lolol','lolo@coucou.com','llk');
